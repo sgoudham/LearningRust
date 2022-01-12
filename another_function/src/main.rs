268453576d5a2d23bbe29c51d3ee4db26e9b491c
Hammy <sgoudham@gmail.com>
2022-01-12 00:27:53 +0000

    Add functions

---
+++ another_function/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("Hello, world!");
+    another_function(5);
+    print_labeled_measurement(5, 'h');
+    println!("The value of x is -> {}", five());
+    println!("The value of x is -> {}", plus_one(5));
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is -> {}", x);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {}{}", value, unit_label);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
